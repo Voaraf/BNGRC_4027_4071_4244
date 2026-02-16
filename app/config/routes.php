f1562e694d86303a2a77b4e48e630bb96e2be13c
Lofo-hub <lofoniaina1325@gmail.com>
2026-02-16 17:25:45 +0300

    gestion

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,13 +2,9 @@
 require_once __DIR__ . '/../controllers/LoginController.php';
 require_once __DIR__ . '/../services/Validator.php';
 require_once __DIR__ . '/../repositories/UserRepository.php';
-require_once __DIR__ . '/../repositories/MessageRepository.php';
-require_once __DIR__ . '/../controllers/MessagesController.php'; 
 
 
 Flight::route('GET /', ['LoginController', 'showLogin']);
 Flight::route('POST /message', ['LoginController', 'postLogin']);
-Flight::route('GET /messages', ['MessagesController', 'showMessages']);
-Flight::route('POST /messages', ['MessagesController', 'postMessages']);
 
 
