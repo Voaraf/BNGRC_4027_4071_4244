267fba0573a3e70ff4a42f3f9386c0d4f1220180
Voaraf <voarafina@gmail.com>
2026-02-17 01:22:41 +0300

    v2

--- app/config/routes.php
+++ app/config/routes.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/../repositories/DonRepository.php';
 require_once __DIR__ . '/../repositories/DashboardRepository.php';
 require_once __DIR__ . '/../controllers/DashboardController.php';
 require_once __DIR__ . '/../repositories/UtilRepository.php';
+require_once __DIR__ . '/../controllers/AchatsController.php';
 
 
 Flight::route('GET /', ['LoginController', 'showLogin']);
